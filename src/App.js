a0a936a452b9c149feb3c45b034709c0eba01d5a
Jose A Gonzalez <JGman13@gmail.com>
2022-09-26 13:14:14 -1000

    checklist 03

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ class App extends React.Component {
       location: {},
       error: false,
       errorMessage: '',
+      lat: "", 
+      lon: "",
     };
   }
 
@@ -23,9 +25,9 @@ class App extends React.Component {
       const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_IQ_KEY}&q=${this.state.searchQuery}&format=json`
       const res = await axios.get(API);
       console.log(res.data);
-      this.setState({ location: res.data[0] });
-      this.setState({ lat: res.data[0] });
-      this.setState({ lon: res.data[0] });
+      this.setState({ location: res.data[0].display_name });
+      this.setState({ lat: res.data[0].lat });
+      this.setState({ lon: res.data[0].lon });
 
     } catch (error) {
       console.log(error);
@@ -43,9 +45,9 @@ class App extends React.Component {
         {
           this.state.location.place_id &&
           <>
-            <h2 className="p-3 mb-2 bg-primary text-white">The City is: {this.state.location.display_name}</h2>
-            <h2 className="p-3 mb-2 bg-primary text-white">The lat is: {this.state.location.lat}</h2>
-            <h2 className="p-3 mb-2 bg-primary text-white">The lon is: {this.state.location.lon}</h2>
+            <h2 className="p-3 mb-2 bg-primary text-white">The City is: {this.state.location}</h2>
+            <h2 className="p-3 mb-2 bg-primary text-white">The lat is: {this.state.lat}</h2>
+            <h2 className="p-3 mb-2 bg-primary text-white">The lon is: {this.state.lon}</h2>
           </>
         }
         {this.state.error &&
@@ -53,7 +55,9 @@ class App extends React.Component {
             <h2 class="p-3 mb-2 bg-danger text-white">Oh no! This City doesn't exist: {this.state.errorMessage}</h2>
           </>
         }
-        < img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_IQ_KEY}&center=${this.state.lat},${this.state.lon}&zoom=13`} alt="Map of City"> Look here:</img>
+        {this.state.lat &&
+        <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_IQ_KEY}&center=${this.state.lat},${this.state.lon}&zoom=13`} alt="Map of City"></img>
+      }
       </>
     );
   };
