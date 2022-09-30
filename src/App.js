9d6b29f87e25c075e9acaa125de47a0cb9d0130c
Jose A Gonzalez <JGman13@gmail.com>
2022-09-29 18:46:14 -1000

    lab 08 done

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import Header from './Components/Header';
 import Main from './Components/Main';
 import Footer from './Components/Footer';
 import Weather from "./Components/Weather";
+import Movies from "./Components/Movies"
 import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends React.Component {
 
   render() {
     return (
-        // eslint-disable-next-line react/style-prop-object
-        <div class="has-bg-img w-100 p-3 mx-auto 200">
-          <Header />
-          <img class ="bg-img" src="https://images.unsplash.com/photo-1469854523086-cc02fe5d8800?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGV4cGxvcmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60 w-50 p-3 200" alt="city"></img>
+      // eslint-disable-next-line react/style-prop-object
+      <div className="has-bg-img w-100 p-3 mx-auto 200">
+        <Header />
+        <img className="bg-img" src="https://images.unsplash.com/photo-1469854523086-cc02fe5d8800?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGV4cGxvcmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60 w-50 p-3 200" alt="city"></img>
         <Main />
         <Weather />
+        <Movies />
         <Footer />
       </div>
     );
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import CityCard from "../Components/CityCard";
 import CityForm from "../Components/CityForm";
 import Weather from '../Components/Weather';
+import Movies from "./Movies";
 
 
 class Main extends React.Component {
@@ -19,6 +20,8 @@ class Main extends React.Component {
       displayCard: false,
       cityMap: '',
       weatherData: [],
+      movieData: [],
+      displayMovies: false, 
       displayWeather: false,
     };
   }
@@ -27,22 +30,7 @@ class Main extends React.Component {
     e.preventDefault();
     this.setState({ searchQuery: e.target.value });
   }
-  handleWeather = async () => {
-    try {
-      const API = `http://localhost:3001/weather?searchQuery=${this.state.searchQuery}&lat=${this.state.location.lat}&lon=${this.state.location.lon}`;
-      const weatherRes = await axios.get(API);
-      this.setState({
-        weatherData: weatherRes.data,
-        displayWeather: true,
-      });
-    } catch (error) {
-      this.setState({error: true,
-        displayWeather: false});
-        this.setState({ displayCard: false, });
-        this.setState({ errorMessage: error.response.status + error.message + ""});
-    }
-  }
-  
+
   handleSearch = async (e) => {
     e.preventDefault();
     this.setState({ error: false });
@@ -55,20 +43,56 @@ class Main extends React.Component {
       this.setState({ lon: res.data[0].lon });
       this.setState({ cityMap: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_IQ_KEY}&center=${res.data[0].lat},${res.data[0].lon}&zoom=12` });
       this.setState({ displayCard: true });
+      this.handleWeather(res.data[0].lat, res.data[0].lon);
+      this.handleMovies();
       this.setState({ searchQuery: '' });
-      this.handleWeather();
-      
+
     } catch (error) {
       console.log(error)
       this.setState({ error: true });
-      this.setState({ errorMessage: error.response.status + error.message + ""});
+      this.setState({ errorMessage: error.response.status + error.message + "" });
       this.setState({ displayCard: false, });
       this.handleWeather();
     }
   }
-    
-    render() {
-      return (
+  handleWeather = async (lat, lon) => {
+    try {
+      const API = `http://localhost:3001/weather?searchQuery=${this.state.searchQuery}&lat=${lat}&lon=${lon}`;
+      console.log(API);
+      console.log(this.state.lat);
+      const weatherRes = await axios.get(API);
+      this.setState({
+        weatherData: weatherRes.data,
+        displayWeather: true,
+      });
+    } catch (error) {
+      this.setState({
+        error: true,
+        displayWeather: false
+      });
+      this.setState({ displayCard: false, });
+      this.setState({ errorMessage: error.response.status + error.message + "" });
+    }
+  }
+  handleMovies = async () => {
+    try {
+      const API = `http://localhost:3001/movies?searchQuery=${this.state.searchQuery}`;
+      const movieRes = await axios.get(API);
+      this.setState({
+        movieData: movieRes.data,
+        displayMovies: true,
+      });
+    } catch (error) {
+      this.setState({
+        error: true,
+        displayMovies: false,
+      });
+      this.setState({ displayCard: false, });
+      this.setState({ errorMessage: error.response.status + error.message + "" });
+    }
+  }
+  render() {
+    return (
       <>
         <CityForm
           handleInput={this.handleInput}
@@ -76,7 +100,7 @@ class Main extends React.Component {
           errorMessage={this.state.errorMessage}
           error={this.state.error}
           searchQuery={this.state.searchQuery}
-          />
+        />
         <CityCard
           displayCard={this.state.displayCard}
           cityName={this.state.location}
@@ -89,6 +113,12 @@ class Main extends React.Component {
         <Weather
           weatherData={this.state.weatherData}
           displayWeather={this.state.displayWeather}
+          errorMessage={this.state.errorMessage}
+          error={this.state.error}
+        />
+        <Movies
+          movieData={this.state.movieData}
+          displayMovies={this.state.displayMovies}
           errorMessage={this.state.errorMessage}
           error={this.state.error}
         />
